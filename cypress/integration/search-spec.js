772d87dcd1fd532c2223f8e85c2c8da80e2beab1
Murat Ozcan <muratkerem@gmail.com>
2019-05-18 14:13:08 -0500

    test(2searches): added a test for back to back search

--- cypress/integration/search-spec.js
+++ cypress/integration/search-spec.js
@@ -4,15 +4,18 @@ import { searchBtn } from './../support/main-page.helper';
 describe('search workflow', function () {
   Cypress.env('RETRIES', 3);
 
-  it('should fill out the form, trigger search and get at least 1 result (if possible)', function () {
-
+  beforeEach('login, fill form and search', function() {
     cy.uiLogin();
     cy.fillSearchForm('Tokyo, Japan');
     searchBtn().should('be.visible').click();
     cy.wait('@destination', { timeout: 15000 });
     // for this timeout we need a System NFR requirement indicating how fast the search results should appear
     // setting it to high timeout because we do not want the tests to fail intermittently
-    cy.log('assert on the search result page');
+  });
+
+  it('should fill out the form, trigger search and get at least 1 result (if possible)', function () {
+
+    cy.log('state: we have logged in, searched and we are asserting the results');
     cy.url().should('include', 'search');
 
     // the oracle is uncertain; there can be results or no results. For this we have to resort to conditional testing only assert if there are results
@@ -27,15 +30,29 @@ describe('search workflow', function () {
           .should('be.gte', 1);
 
         // visual ai snapshot here is experimental, we might need to find a way to tell the system to ignore the different KIND of results that can populate
+        // will need to talk to development to find out if there is any value
+        // one area to test could be localization, but this was not a requirement. This is to show that these problems are solvable with visual ai
         cy.percySnapshot('Search Result Snapshot');
       } else {
         Cypress.log({
           name: 'Search did not yield results, no assertions were made'
         });
       }
-
     });
 
+  });
+
+  it('should do 2 searches back to back', function () {
+    cy.log('state: we have logged in, did one search and got results. We are doing a 2nd search ');
+
+    cy.selectDestination();
+    cy.selectRewards();
+
+    searchBtn().should('be.visible').click();
+
+    cy.get('.hotel-result-container > ')
+      .its('length')
+      .should('be.gte', 1);
   });
 
 });
--- cypress/support/main-page.helper.js
+++ cypress/support/main-page.helper.js
@@ -29,4 +29,4 @@ export const rooms = () => cy.get('.rooms');
 export const roomDropdown = () => cy.get('.rooms > .rm-select-base > .btn-group > .dropdown-menu');
 export const n_rooms = numRooms => cy.get(`.rooms > .rm-select-base > .btn-group > .dropdown-menu > :nth-child(${numRooms}) > a`);
 
-export const searchBtn = () => cy.get('.rm-btn-orange > .ng-scope');
+export const searchBtn = () => cy.get('.rm-btn-orange');
